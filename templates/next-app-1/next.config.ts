d393bd0f7660fa5e11f7dca96e701b9cef3d8269
David Chan <66018863+Duc-Developer@users.noreply.github.com>
2025-03-10 23:31:12 +0700

    feat: update language + theme for nextapp1 (#13)

--- templates/next-app-1/next.config.ts
+++ templates/next-app-1/next.config.ts
@@ -1,5 +1,6 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
@@ -13,5 +14,5 @@ const nextConfig: NextConfig = {
     additionalData: `@use 'src/styles/_variables' as *;`,
   },
 };
-
-export default bundleAnalyzer(nextConfig);
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(bundleAnalyzer(nextConfig));
