bfe17114cc6836e8c9ea8e5069420c9661d387fd
David Chan <mrtranduc1994@outlook.com>
2025-03-10 23:30:19 +0700

    feat: update language + theme for nextapp1

--- templates/next-app-1/src/app/[locale]/home/page.tsx
+++ templates/next-app-1/src/app/[locale]/home/page.tsx
@@ -1,11 +1,13 @@
+import { useTranslations } from 'next-intl';
 import React from 'react';
 
 import './style.scss';
 
 const Home = () => {
+  const t = useTranslations();
   return (
     <div className='home-page'>
-      <h1 className='text-4xl font-bold'>Home page</h1>
+      <h1 className='text-4xl font-bold'>{t('homePage.title')}</h1>
       <p>This is project based on nextjs + bun + tailwind + scss</p>
     </div>
   );
